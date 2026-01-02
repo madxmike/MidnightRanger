6a763abef29fd3218a2f56f9144e6734a9758709
Michael Epps <mep053@gmail.com>
2026-01-01 23:21:21 -0500

    Extract texture loading and uploading to a function

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_log.h"
 #include "SDL3/SDL_video.h"
-#include <iostream>
-#include <ostream>
 #include <string>
 
 constexpr int WindowWidth = 800;
@@ -61,78 +59,84 @@ SDL_Context initSDL() {
     };
 }
 
+SDL_GPUTexture* LoadAndCreateTexture(SDL_GPUDevice *device, std::string fileName) {
+    const std::string basePath = SDL_GetBasePath();
+    const std::string filePath = basePath + "Content/Images/" + "test-sprite.png";
+
+    SDL_Surface* surface = IMG_Load(filePath.c_str());
+    if (surface == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR,
+                     "Could not load texture %s: %s\n", filePath.c_str(),  SDL_GetError());
+        return nullptr;
+    }
+
+    const Uint32 textureWidth = static_cast<Uint32>(surface->w);
+    const Uint32 textureHeight = static_cast<Uint32>(surface->h);
+
+    SDL_GPUTransferBufferCreateInfo transferBufferCreateInfo = {
+        .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+        .size = textureWidth * textureHeight * 4,
+    };
+    SDL_GPUTransferBuffer *transferBuffer = SDL_CreateGPUTransferBuffer(device, &transferBufferCreateInfo);
+    {
+        void *buffer = SDL_MapGPUTransferBuffer(device, transferBuffer, false);
+        SDL_memcpy(buffer, surface->pixels, textureWidth * textureHeight * 4);
+        SDL_UnmapGPUTransferBuffer(device, transferBuffer);
+    }
+
+    SDL_DestroySurface(surface);
+
+    SDL_GPUTextureCreateInfo textureCreateInfo = {
+        .type = SDL_GPU_TEXTURETYPE_2D,
+        .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
+        .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
+        .width = textureWidth,
+        .height = textureWidth,
+        .layer_count_or_depth = 1,
+        .num_levels = 1,
+    };
+
+    SDL_GPUTexture *texture = SDL_CreateGPUTexture(device, &textureCreateInfo);
+
+    SDL_GPUCommandBuffer *commandBuffer = SDL_AcquireGPUCommandBuffer(device);
+    if (commandBuffer == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR,
+                    "Could not acquire command buffer: %s\n", SDL_GetError());
+        return nullptr;
+    }
+
+    SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(commandBuffer);
+    if (copyPass == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR,
+                    "Could not acquire copy pass: %s\n", SDL_GetError());
+        return nullptr;
+    }
+
+    SDL_GPUTextureTransferInfo transferBufferLocation = {
+        .transfer_buffer = transferBuffer,
+        .offset = 0,
+    };
+    SDL_GPUTextureRegion textureRegion = {
+        .texture = texture,
+        .w = textureWidth,
+        .h = textureHeight,
+        .d = 1,
+    };
+
+    SDL_UploadToGPUTexture(copyPass, &transferBufferLocation, &textureRegion, false);
+
+    SDL_EndGPUCopyPass(copyPass);
+    SDL_SubmitGPUCommandBuffer(commandBuffer);
+    SDL_ReleaseGPUTransferBuffer(device, transferBuffer);
+
+    return texture;
+}
+
 int main() {
 
     SDL_Context sdlContext = initSDL();
 
-  const std::string basePath = SDL_GetBasePath();
-  const std::string filePath = basePath + "Content/Images/" + "test-sprite.png";
-
-  std::cout << "Loading image " << filePath << std::endl;
-  SDL_Surface* surface = IMG_Load(filePath.c_str());
-  if (surface == nullptr) {
-      SDL_LogError(SDL_LOG_CATEGORY_ERROR,
-                   "Could not load surface: %s\n", SDL_GetError());
-      return -1;
-  }
-  const Uint32 textureWidth = static_cast<Uint32>(surface->w);
-  const Uint32 textureHeight = static_cast<Uint32>(surface->h);
-
-  SDL_GPUTextureCreateInfo textureCreateInfo = {
-      .type = SDL_GPU_TEXTURETYPE_2D,
-      .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
-      .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER,
-      .width = textureWidth,
-      .height = textureWidth,
-      .layer_count_or_depth = 1,
-      .num_levels = 1,
-  };
-
-  SDL_GPUTexture *texture = SDL_CreateGPUTexture(sdlContext.device, &textureCreateInfo);
-
-  SDL_GPUTransferBufferCreateInfo transferBufferCreateInfo = {
-      .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
-      .size = textureWidth * textureHeight * 4,
-  };
-
-  SDL_GPUTransferBuffer *textureTransferBuffer = SDL_CreateGPUTransferBuffer(sdlContext.device, &transferBufferCreateInfo);
-
-  {
-      SDL_GPUCommandBuffer *commandBuffer = SDL_AcquireGPUCommandBuffer(sdlContext.device);
-      if (commandBuffer == nullptr) {
-          SDL_LogError(SDL_LOG_CATEGORY_ERROR,
-                      "Could not acquire copy pass command buffer: %s\n", SDL_GetError());
-          return -1;
-      }
-
-      void *buffer = SDL_MapGPUTransferBuffer(sdlContext.device, textureTransferBuffer, false);
-      SDL_memcpy(buffer, surface->pixels, textureWidth * textureHeight * 4);
-      SDL_UnmapGPUTransferBuffer(sdlContext.device, textureTransferBuffer);
-
-      SDL_GPUCopyPass* copyPass = SDL_BeginGPUCopyPass(commandBuffer);
-      if (copyPass == nullptr) {
-          SDL_LogError(SDL_LOG_CATEGORY_ERROR,
-                      "Could not acquire copy pass: %s\n", SDL_GetError());
-          return -1;
-      }
-      SDL_GPUTextureTransferInfo transferBufferLocation = {
-          .transfer_buffer = textureTransferBuffer,
-          .offset = 0,
-      };
-      SDL_GPUTextureRegion textureRegion = {
-          .texture = texture,
-          .w = textureWidth,
-          .h = textureHeight,
-          .d = 1,
-      };
-
-      SDL_UploadToGPUTexture(copyPass, &transferBufferLocation, &textureRegion, false);
-      SDL_EndGPUCopyPass(copyPass);
-      SDL_SubmitGPUCommandBuffer(commandBuffer);
-
-      SDL_ReleaseGPUTransferBuffer(sdlContext.device, textureTransferBuffer);
-      SDL_DestroySurface(surface);
-  }
+    SDL_GPUTexture *texture = LoadAndCreateTexture(sdlContext.device, "test_sprite.png");
 
   SDL_GPUSamplerCreateInfo samplerCreateInfo = {
       .min_filter = SDL_GPU_FILTER_NEAREST,
